b48fe9a1670d9e14c6403d48c5eaaa7ad25cc3a5
LENOVO.USER <ei.yanezs@uea.edu.ec>
2025-03-16 20:36:41 -0500

    Carpeta con tarea de la semana 13

---
+++ Semana 13/Programa para calcular temperaturas promedio.py
@@ -0,0 +1,22 @@
+def calcular_temperatura_promedio(datos_ciudades):
+
+    promedios = {}
+
+    for ciudad, temperaturas in datos_ciudades.items():
+        #promedio de las temperaturas
+        promedio = sum(temperaturas) / len(temperaturas)
+        promedios[ciudad] = promedio
+
+    return promedios
+
+datos_ciudades = {
+    "Quito": [18, 19, 20, 18],  #temperaturas de quito
+    "Manta": [28, 30, 29, 27],  #temperaturas de manta
+    "Tena": [22, 24, 25, 28]  #temperaturas de tena
+}
+
+resultados = calcular_temperatura_promedio(datos_ciudades)
+
+print("Temperaturas Promedio por Ciudad:")
+for ciudad, promedio in resultados.items():
+    print(f"{ciudad}: {promedio}°C")
